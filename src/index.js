e6572c5ba1de250850838c7155f488d9711a4798
rydvone <rydvone@gmail.com>
2022-01-31 02:21:23 +0300

    feat: use concat

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  const mtx = JSON.parse(JSON.stringify(matrix));
-  let a;
-  let arr = [];
-  let arr2 = [];
-  let arrRev = [];
-  for (let i = 0; i < m1.length; i++) {
-    if (i % 2 == 0) {
-      arr.push(mtx[i].slice());
-    } else {
-      arrRev = [];
+module.exports = function towelSort(matrix) {
+  if (!(matrix)) {
+    return [];
+  } else {
+    let arr = [];
+    for (let i = 0; i < matrix.length; i++) {
       if (i % 2 != 0) {
-        for (let j = (mtx[i].length - 1); j >= 0; j--) {
-          console.log(mtx[i][j]);
-          a = mtx[i][j]
-          arrRev.push(a);
+        for (let j = (matrix[i].length - 1); j >= 0; j--) {
+          arr = arr.concat(matrix[i][j])
         }
+      } else {
+        arr = arr.concat(matrix[i])
       }
-      console.log(arrRev);
-      arr.push(arrRev.slice());
     }
-         
+    return arr;
   }
-  for (let items of arr) {
-    arr2 = arr2.concat(items);
-  }
-  return arr2;
-  // return [];
 }
+
