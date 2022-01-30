2de3dec4a35e9ae8d7d3bdd5d59bf9548a6828ec
rydvone <rydvone@gmail.com>
2022-01-31 01:40:13 +0300

    feat: for and if method

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const mtx = JSON.parse(JSON.stringify(matrix));
+  let a;
+  let arr = [];
+  let arr2 = [];
+  let arrRev = [];
+  for (let i = 0; i < m1.length; i++) {
+    if (i % 2 == 0) {
+      arr.push(mtx[i].slice());
+    } else {
+      arrRev = [];
+      if (i % 2 != 0) {
+        for (let j = (mtx[i].length - 1); j >= 0; j--) {
+          console.log(mtx[i][j]);
+          a = mtx[i][j]
+          arrRev.push(a);
+        }
+      }
+      console.log(arrRev);
+      arr.push(arrRev.slice());
+    }
+         
+  }
+  for (let items of arr) {
+    arr2 = arr2.concat(items);
+  }
+  return arr2;
+  // return [];
 }
